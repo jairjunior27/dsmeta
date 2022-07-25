b768ea59c292e098655fe9de08e7d582a943c58d
Jair Junior <radiovisao@ig.com.br>
2022-07-25 10:38:01 -0300

    Ajuste de dados

---
+++ README.md
@@ -0,0 +1,36 @@
+<h1 align="center">DSMeta</h1>
+
+
+Tabela de conteúdos
+=================
+<!--ts-->
+   * [Sobre](#Sobre)
+   * [Status do projeto](#status-do-projeto)
+   * [Demonstração da aplicação](#Demonstração-da-aplicação)
+   * [Como usar](#como-usar)
+   * [Tecnologias](#tecnologias)
+<!--te-->
+
+<h2>Sobre</h2>
+<p>Projeto desenvolvido durante a Semana Spring React 9 da DevSuperior em que foi criada uma aplicação de listagem de vendas e envio de notificações.</p>
+
+<h2>Status do projeto</h2>
+Concluido.
+
+<h2>Demonstração da aplicação</h2>
+
+![dsmeta1](https://user-images.githubusercontent.com/77522739/180333915-cbda63b2-f84c-429a-a5d9-7fea7ebae360.png)
+
+
+<h2>Como usar</h2>
+<p>Clique no link: <a href="https://dsmeta-jair.netlify.app">DSMeta</a>, escolha o périodo de vendas em que quer visualizar clicando nos campos de datas e clique no ícone para enviar uma notificação da venda/vendedor escolhido.</p>
+
+<h2>Tecnologias</h2>
+<div>
+  <img align="center" alt="spring" pringheight="40" width="50" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/spring/spring-original-wordmark.svg" />
+  <img align="center" alt="java" height="40" width="50" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original-wordmark.svg" />
+  <img align="center" alt="react" height="40" width="50" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg">
+  <img align="center" alt="typescript" height="40" width="50" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" />
+  <img align="center" alt="heroku" height="40" width="50" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/heroku/heroku-plain-wordmark.svg" />
+</div>
+
---
+++ frontend/src/assets/img/dsmeta_files/main.tsx
@@ -0,0 +1,19 @@
+var _jsxFileName = "C:\\projetos\\dsmeta\\frontend\\src\\main.tsx";
+import __vite__cjsImport0_react from "/node_modules/.vite/deps/react.js?v=706b773a"; const React = __vite__cjsImport0_react.__esModule ? __vite__cjsImport0_react.default : __vite__cjsImport0_react;
+import __vite__cjsImport1_reactDom_client from "/node_modules/.vite/deps/react-dom_client.js?v=706b773a"; const ReactDOM = __vite__cjsImport1_reactDom_client.__esModule ? __vite__cjsImport1_reactDom_client.default : __vite__cjsImport1_reactDom_client;
+import App from "/src/App.tsx";
+import "/src/index.css";
+import { jsxDEV as _jsxDEV } from "/@id/__x00__react/jsx-dev-runtime";
+ReactDOM.createRoot(document.getElementById("root")).render(/* @__PURE__ */ _jsxDEV(React.StrictMode, {
+  children: /* @__PURE__ */ _jsxDEV(App, {}, void 0, false, {
+    fileName: _jsxFileName,
+    lineNumber: 8,
+    columnNumber: 5
+  }, this)
+}, void 0, false, {
+  fileName: _jsxFileName,
+  lineNumber: 7,
+  columnNumber: 3
+}, this));
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJtYXBwaW5ncyI6IjtBQUFBO0FBQ0E7QUFDQTtBQUNBOztBQUVBQSxTQUFTQyxXQUFXQyxTQUFTQyxlQUFlLE1BQXhCLENBQXBCLEVBQW9FQyxPQUNsRSx3QkFBQyxNQUFNLFlBQVA7QUFBQSxZQUNFLHdCQUFDLEtBQUQ7QUFBQTtBQUFBO0FBQUE7QUFBQTtBQURGO0FBQUE7QUFBQTtBQUFBO0FBQUEsUUFERiIsIm5hbWVzIjpbIlJlYWN0RE9NIiwiY3JlYXRlUm9vdCIsImRvY3VtZW50IiwiZ2V0RWxlbWVudEJ5SWQiLCJyZW5kZXIiXSwic291cmNlcyI6WyJDOi9wcm9qZXRvcy9kc21ldGEvZnJvbnRlbmQvc3JjL21haW4udHN4Il0sImZpbGUiOiJDOi9wcm9qZXRvcy9kc21ldGEvZnJvbnRlbmQvc3JjL21haW4udHN4Iiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IFJlYWN0IGZyb20gJ3JlYWN0J1xuaW1wb3J0IFJlYWN0RE9NIGZyb20gJ3JlYWN0LWRvbS9jbGllbnQnXG5pbXBvcnQgQXBwIGZyb20gJy4vQXBwJ1xuaW1wb3J0ICcuL2luZGV4LmNzcydcblxuUmVhY3RET00uY3JlYXRlUm9vdChkb2N1bWVudC5nZXRFbGVtZW50QnlJZCgncm9vdCcpIGFzIEhUTUxFbGVtZW50KS5yZW5kZXIoXG4gIDxSZWFjdC5TdHJpY3RNb2RlPlxuICAgIDxBcHAgLz5cbiAgPC9SZWFjdC5TdHJpY3RNb2RlPlxuKVxuIl19
